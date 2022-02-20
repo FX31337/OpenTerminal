675e882964797c5d881e3f8e57fd21034c78b12f
kenorb <kenorb@users.noreply.github.com>
2022-02-20 16:59:37 +0000

    Adds more options to Next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
+  assetPrefix: process.env.BASE_PATH,
+  basePath: process.env.BASE_PATH,
+  distDir: process.env.BUILD_PATH,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client.
+    staticFolder: '/static',
+  },
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    // Will only be available on the server side.
+  },
 }
 
 module.exports = nextConfig
